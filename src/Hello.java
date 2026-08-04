3fada0150c9366ef724ec06f6201d47b69bc0d75
Velpula Ashwini <vvelpulaashwini@gmail.com>
2026-08-04 18:30:38 +0530

    Updated output

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Welcome to jenkins CI/CD Demo");
+        System.out.println("continuous integration successful");
     }
 }
